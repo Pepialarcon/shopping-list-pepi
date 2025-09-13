cb53334851840812b3df9eded12350e0d0aca84f
Pepialarcon <mdafrica24@gmail.com>
2025-09-13 14:13:33 +0200

    feat: create function additem

--- Shoppinglistsinayuda.js
+++ Shoppinglistsinayuda.js
@@ -2,7 +2,7 @@ let shoppingList = []
 
 let firstProduct = { 
     item: "pan",
-    quality: 2,
+    quantity: 2,
     purchased: false
 } 
 
@@ -10,24 +10,34 @@ shoppingList.push(firstProduct)
 
 let secondProduct ={
     item: "leche",
-    quality: 3,
+    quantity: 3,
     purchased: false
 } 
 shoppingList.push(secondProduct)
 
 let thirdProduct ={
     item: "arroz",
-    quality: 1,
+    quantity: 1,
     purchased: false
 } 
 shoppingList.push(thirdProduct)
 let fourProduct ={
     item: "cafe",
-    quality: 4,
+    quantity: 4,
     purchased: false
 } 
 shoppingList.push(fourProduct)
+function addItem(list/*shoppinglist */, item, quantity){
+    let newProduct ={
+        item: item, //item: "Coca cola"
+        quantity: quantity,
+        purchased: false
+    }
+list.push(newProduct)
+return list
+}
 
+shoppingList = addItem(shoppingList,"sandia", 4) //shoppingList le asigna el resultado de ejecturar addItem)//
 console.table(shoppingList)
 
 
