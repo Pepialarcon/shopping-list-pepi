3f22ceb50e4c0852590f0de7dd8ef470a40ce659
Pepialarcon <mdafrica24@gmail.com>
2025-09-13 13:26:54 +0200

    feat: create shoppingList array with four objects

---
+++ Shoppinglistsinayuda.js
@@ -0,0 +1,34 @@
+let shoppingList = []
+
+let firstProduct = { 
+    item: "pan",
+    quality: 2,
+    purchased: false
+} 
+
+shoppingList.push(firstProduct)
+
+let secondProduct ={
+    item: "leche",
+    quality: 3,
+    purchased: false
+} 
+shoppingList.push(secondProduct)
+
+let thirdProduct ={
+    item: "arroz",
+    quality: 1,
+    purchased: false
+} 
+shoppingList.push(thirdProduct)
+let fourProduct ={
+    item: "cafe",
+    quality: 4,
+    purchased: false
+} 
+shoppingList.push(fourProduct)
+
+console.table(shoppingList)
+
+
+
